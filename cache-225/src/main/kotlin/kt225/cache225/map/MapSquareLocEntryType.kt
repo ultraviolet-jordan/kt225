89340be8aa349f5cee5bc89b57a53311fa042219
Jordan <personal.jordanabraham@gmail.com>
2023-06-21 19:55:19 -0400

    Add functions to modify locs of a mapsquare entry.

--- cache-225/src/main/kotlin/kt225/cache225/map/MapSquareLocEntryType.kt
+++ cache-225/src/main/kotlin/kt225/cache225/map/MapSquareLocEntryType.kt
@@ -1,6 +1,7 @@
 package kt225.cache225.map
 
 import kt225.cache.EntryType
+import kt225.common.game.world.Position
 
 /**
  * @author Jordan Abraham
@@ -8,4 +9,41 @@ import kt225.cache.EntryType
 data class MapSquareLocEntryType(
     val mapSquare: Int,
     val locs: MutableMap<Int, Array<Long?>> = HashMap()
-) : EntryType
+) : EntryType {
+    fun query(position: Position, function: (result: List<MapSquareLoc>, local: MapSquareLocalPosition) -> Unit) {
+        val mapSquare = MapSquare(mapSquare)
+        val baseX = mapSquare.x shl 6
+        val baseZ = mapSquare.z shl 6
+        val localPosition = MapSquareLocalPosition(position.plane, position.x - baseX, position.z - baseZ)
+        val mapSquareLocs = locs[localPosition.packed]!!.mapNotNull { it?.let(::MapSquareLoc) }
+        function.invoke(mapSquareLocs, localPosition)
+    }
+    
+    fun removeLoc(loc: MapSquareLoc, local: MapSquareLocalPosition): Boolean {
+        val locs = locs[local.packed] ?: return false
+        val index = locs.indexOf(loc.packed)
+        if (index == -1) {
+            return false
+        }
+        locs[index] = null
+        return true
+    }
+
+    fun addLoc(loc: MapSquareLoc, local: MapSquareLocalPosition): Boolean {
+        val existing = locs[local.packed]
+        if (existing != null) {
+            val index = existing.indexOf(loc.packed)
+            if (index == -1) { // If new loc on this pos.
+                val length = existing.size
+                val clone = existing.copyOf(length + 1)
+                clone[length] = loc.packed
+                locs[local.packed] = clone
+                return true
+            }
+            existing[index] = loc.packed
+            return true
+        }
+        locs[local.packed] = Array(1) { loc.packed }
+        return locs[local.packed] != null
+    }
+}
