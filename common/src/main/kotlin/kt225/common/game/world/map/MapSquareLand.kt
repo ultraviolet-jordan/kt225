111c0dbd8523aae3628606b7babe6e6c7d3c3e43
Jordan <personal.jordanabraham@gmail.com>
2023-07-01 17:44:53 -0400

    Document MapSquareLand.

--- common/src/main/kotlin/kt225/common/game/world/map/MapSquareLand.kt
+++ common/src/main/kotlin/kt225/common/game/world/map/MapSquareLand.kt
@@ -1,6 +1,34 @@
 package kt225.common.game.world.map
 
 /**
+ * ```
+ * ==================================================================================
+ * | PROPERTY         | DECIMAL | HEX  |  BINARY  | DIGITS | ========================
+ * ==================================================================================
+ * | HEIGHT           | 255     | 0xff | 11111111 | 8      | ========================
+ * ==================================================================================
+ * | OVERLAY_ID       | 127     | 0x7f | 1111111  | 7      | ========================
+ * ==================================================================================
+ * | OVERLAY_PATH     | 31      | 0x1f | 11111    | 5      | ========================
+ * ==================================================================================
+ * | OVERLAY_ROTATION | 3       | 0x3  | 11       | 2      | ========================
+ * ==================================================================================
+ * | COLLISION        | 31      | 0x1f | 11111    | 5      | ========================
+ * ==================================================================================
+ * | UNDERLAY_ID      | 127     | 0x7f | 1111111  | 7      | ========================
+ * ==================================================================================
+ * ==================================================================================
+ * ==================================================================================
+ * | CAPACITY | 17179869183 | 0x3ffffffff | 1111111111111111111111111111111111 | 34 |
+ * ==================================================================================
+ * ```
+ *
+ * <b>An example of the highest possible bit-packed land.</b>
+ * ```
+ * val land = MapSquareLand(255, 127, 31, 3, 31, 127)
+ * assert(0x3ffffffff == land.packed)
+ * ```
+ * 
  * @author Jordan Abraham
  */
 @JvmInline
@@ -16,7 +44,7 @@ value class MapSquareLand(
         underlayId: Int
     ) : this(
         (height.toLong() and HEIGHT_MASK)
-            or (overlayId.toLong() and OVERLAY_MASK shl OVERLAY_BITS)
+            or (overlayId.toLong() and OVERLAY_ID_MASK shl OVERLAY_ID_BITS)
             or (overlayPath.toLong() and OVERLAY_PATH_MASK shl OVERLAY_PATH_BITS)
             or (overlayRotation.toLong() and OVERLAY_ROTATION_MASK shl OVERLAY_ROTATION_BITS)
             or (collision.toLong() and COLLISION_MASK shl COLLISION_BITS)
@@ -40,7 +68,7 @@ value class MapSquareLand(
      * The overlay of this land on a MapSquare coordinate.
      */
     inline val overlayId: Int 
-        get() = (packed shr OVERLAY_BITS and OVERLAY_MASK).toInt()
+        get() = (packed shr OVERLAY_ID_BITS and OVERLAY_ID_MASK).toInt()
 
     /**
      * The overlay path of this land on a MapSquare coordinate.
@@ -68,15 +96,15 @@ value class MapSquareLand(
     
     companion object {
         const val HEIGHT_MASK = 0xffL
-        const val OVERLAY_BITS = 8
-        const val OVERLAY_MASK = 0x7fL
+        const val OVERLAY_ID_BITS = 8
+        const val OVERLAY_ID_MASK = 0x7fL
         const val OVERLAY_PATH_BITS = 15
         const val OVERLAY_PATH_MASK = 0x1fL
         const val OVERLAY_ROTATION_BITS = 20
         const val OVERLAY_ROTATION_MASK = 0x3L
-        const val COLLISION_BITS = 23
+        const val COLLISION_BITS = 22
         const val COLLISION_MASK = 0x1fL
-        const val UNDERLAY_BITS = 28
+        const val UNDERLAY_BITS = 27
         const val UNDERLAY_MASK = 0x7fL
     }
 }
