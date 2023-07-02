356094529e6199e5e4820448098910baff65e2ff
Jordan <personal.jordanabraham@gmail.com>
2023-07-02 06:41:19 -0400

    Inline LoadAreaPacket.

--- packet/src/main/kotlin/kt225/packet/type/server/LoadAreaPacket.kt
+++ packet/src/main/kotlin/kt225/packet/type/server/LoadAreaPacket.kt
@@ -5,7 +5,24 @@ import kt225.common.packet.Packet
 /**
  * @author Jordan Abraham
  */
-data class LoadAreaPacket(
-    val zoneX: Int,
-    val zoneZ: Int
-) : Packet
+@JvmInline
+value class LoadAreaPacket(
+    val packed: Int
+) : Packet {
+    constructor(
+        zoneX: Int,
+        zoneZ: Int
+    ) : this(
+        zoneX and 0x3ff
+            or (zoneZ and 0x3ff shl 10)
+    ) {
+        require(this.zoneX == zoneX)
+        require(this.zoneZ == zoneZ)
+    }
+    
+    inline val zoneX: Int
+        get() = packed and 0x3ff
+    
+    inline val zoneZ: Int
+        get() = packed shr 10 and 0x3ff
+}
